9b9164a651211dd10e29fb5c1c458acabb6a9141
Euiseo Nam <101988147+2021147566@users.noreply.github.com>
2024-12-09 04:15:25 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,14 @@
-# setup.sh
+#!/bin/bash
+
+# Google Chrome 설치
 apt-get update && apt-get install -y wget gnupg unzip
 wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | apt-key add -
 echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google-chrome.list
 apt-get update && apt-get install -y google-chrome-stable
-wget https://chromedriver.storage.googleapis.com/114.0.5735.90/chromedriver_linux64.zip
+
+# ChromeDriver 설치
+CHROMEDRIVER_VERSION=`curl -sS chromedriver.storage.googleapis.com/LATEST_RELEASE`
+wget https://chromedriver.storage.googleapis.com/${CHROMEDRIVER_VERSION}/chromedriver_linux64.zip
 unzip chromedriver_linux64.zip -d /usr/local/bin/
+chmod +x /usr/local/bin/chromedriver
 rm chromedriver_linux64.zip
